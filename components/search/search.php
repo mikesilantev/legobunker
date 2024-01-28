4ef44061edf0a5aa582817ef9feb292bb99ef5ef
Mike <77847034+mikesilantev@users.noreply.github.com>
2024-01-28 17:19:01 +0300

    layout__search

--- components/search/search.php
+++ components/search/search.php
@@ -1,29 +1,6 @@
 <?php mgAddMeta('components/search/search.js'); ?>
 <?php mgAddMeta('components/search/search.css'); ?>
 
-<div class="c-search">
-
-    <form class="c-search__form c-form" aria-label="<?php echo lang('searchAriaLabel'); ?>" role="search" method="GET"
-        action="<?php echo SITE ?>/catalog">
-
-        <input type="search" autocomplete="off" aria-label="<?php echo lang('searchPh'); ?>" name="search"
-            placeholder="<?php echo lang('searchPh'); ?>"
-            value="<?php if (isset($_GET['search'])) {
-                echo $_GET['search'];
-            } ?>">
-
-        <button type="submit" class="c-button c-search__button">
-            <svg class="icon icon--search">
-                <use xlink:href="#icon--search"></use>
-            </svg>
-            <?php echo lang('search'); ?>
-        </button>
-
-    </form>
-
-
-</div>
-
 <div class="header__search">
 
     <form action="" class="search" aria-label="<?php echo lang('searchAriaLabel'); ?>" role="search" method="GET"
